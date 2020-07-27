9a01875a6c74cee25b444f79a8d36a0006c7b915
Carolina Amaral Chaves <cchaves@ciandt.com>
2020-07-27 20:12:10 -0300

    Fixing AutoMapper

---
+++ CentralDeErros.Api/Controllers/ErrorController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using CentralDeErros.Business.Manager.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CentralDeErros.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        private readonly IErrorManager _errorManager;
+
+        public ErrorController(IErrorManager errorManager)
+        {
+            _errorManager = errorManager;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Business.Models.ErrorDTO>> Get()
+        {
+            try
+            {
+                var response = _errorManager.GetAll();
+
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<Business.Models.ErrorDTO> Create(Business.Models.ErrorDTO error)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _errorManager.Save(error);
+
+            return Ok();
+        }
+    }
+}
--- CentralDeErros.Api/Startup.cs
+++ CentralDeErros.Api/Startup.cs
@@ -1,17 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using CentralDeErros.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using CentralDeErros.Data.Repository.Interfaces;
+using CentralDeErros.Data.Repository;
+using CentralDeErros.Business.Manager;
+using CentralDeErros.Business.Manager.Interfaces;
+
 
 namespace CentralDeErros.Api
 {
@@ -29,6 +27,13 @@ namespace CentralDeErros.Api
         {
             services.AddControllers();
             services.AddDbContext<CentralDeErrosContext>();
+            services.AddScoped<IErrorRepository, ErrorRepository>();
+            services.AddScoped<IErrorManager, ErrorManager>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICategoryManager, CategoryManager>();
+            services.AddScoped<ILevelManager, LevelManager>();
+            services.AddScoped<ILevelRepository, LevelRepository>();
+
             var mappingConfig = new MapperConfiguration(config =>
             {
                 config.AddProfile(new AutoMapperProfile());
@@ -36,6 +41,7 @@ namespace CentralDeErros.Api
 
             IMapper mapper = mappingConfig.CreateMapper();
             services.AddSingleton(mapper);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
