e2831af83719f7cead88ca0f3a8ae12612fc5a8c
Carolina Amaral Chaves <cchaves@ciandt.com>
2020-07-27 20:06:48 -0300

    Fixing AutoMapper

--- CentralDeErros.Api/AutoMapperProfile.cs
+++ CentralDeErros.Api/AutoMapperProfile.cs
@@ -2,6 +2,7 @@
 using CentralDeErros.Business;
 using CentralDeErros.Business.Models;
 using CentralDeErros.Data.Models;
+using System;
 
 namespace CentralDeErros.Api
 {
@@ -9,7 +10,17 @@ namespace CentralDeErros.Api
     {
         public AutoMapperProfile()
         {
-            CreateMap<Error, ErrorDTO>().ReverseMap();
+            CreateMap<Error, ErrorDTO>()
+                .ForMember(dest => dest.Level, act => act.MapFrom(src => src.Level.Name))
+                .ForMember(dest => dest.Category, act => act.MapFrom(src => src.Category.Name))
+                .ForMember(dest => dest.CreatedAt, act => act.MapFrom(src => src.CreatedAt.ToString("MM/dd/yyyy HH:mm:ss")));
+
+            CreateMap<ErrorDTO, Error>()
+                .ForMember(d => d.Level, act => act.Ignore())
+                .ForMember(d => d.Category, act => act.Ignore())
+                .ForMember(d => d.CreatedAt, act => act.MapFrom(src => DateTime.Now));
+
+
             CreateMap<Level, LevelDTO>().ReverseMap();
             CreateMap<Category, CategoryDTO>().ReverseMap();
         }
